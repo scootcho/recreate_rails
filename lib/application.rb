9ebfd322d7a09e503473dcfc032eeb62e6869ff0
Scott Yu <scott.tj.yu@gmail.com>
2015-03-06 09:17:40 +0800

    added routes and load_controller_class

--- lib/application.rb
+++ lib/application.rb
@@ -1,10 +1,27 @@
+require "action_controller"
+require "application_controller"
+
 class Application
   def call(env)
     request = Rack::Request.new(env)
     response = Rack::Response.new
 
-    response.write "hi from Application"
+    controler_name, action_name = route(request.path_info)
+    # ["home", "index"]
     
+    # "home" => HomeController
     response.finish
   end 
+
+  def route(path)
+    # => "home/index" => ["", "home", "index"]
+    _, controller_name, action_name = path.split("/") 
+    [controller_name || "home", action_name || "index" ]
+  end
+
+  def load_controller_class(name)
+    # name = "home" => HomeController
+    require "#{name}_controller" # require "home_controller"
+    Object.const_get name.capitalize + "Controller" # "HomeController" 
+  end
 end
