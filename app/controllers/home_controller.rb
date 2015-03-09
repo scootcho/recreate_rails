0cb7cd129cd4063bdf958bd9c77a76f6553d6fac
Scott Yu <scott.tj.yu@gmail.com>
2015-03-09 21:20:09 +0800

    Implemented after_action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,16 @@
 class HomeController < ApplicationController
   before_action :header #self.before_action or HomeController.before_action
-
+  after_action :footer
+  
   def index
     response.write "Hi from HomeController"    
   end
 
   def header
     response.write "<h1>My App</h1>"
+  end
+
+  def footer
+    response.write "<p>&copy; me</p>"
   end
 end
--- lib/filtering.rb
+++ lib/filtering.rb
@@ -11,10 +11,19 @@ module Filtering
     def before_actions
       @before_actions ||= []  #to gather all the before actions in an array for processing
     end
+
+    def after_action(method)
+      after_actions << method
+    end
+
+    def after_actions
+      @after_actions ||= []  #to gather all the before actions in an array for processing
+    end
   end
 
   def process(action_name) #the process method corresponds to the one in ActionController.
     self.class.before_actions.each { |method| send method }
     super #this calls the ActionController::Base.process
+    self.class.after_actions.each { |method| send method }
   end
 end
